00949103a7337c157633ab514c64c53bfaf55a21
Shan <shannon.mulgrew2@mail.dcu.ie>
2023-07-31 21:24:04 +0100

    code signal q's

---
+++ CodeSignal Questions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeSignal_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
